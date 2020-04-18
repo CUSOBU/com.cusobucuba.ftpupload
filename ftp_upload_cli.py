156821b202d8fca839d8ac4210b1ff0a8e400fdc
fsadannn <fsadannn@gmail.com>
2020-04-18 15:34:35 -0400

    optimize

--- ftp_upload_cli.py
+++ ftp_upload_cli.py
@@ -1,63 +1,75 @@
 import os
-
+from fs import open_fs
+from fs.wrap import read_only, cache_directory
+from fs.walk import Walker
 import fire
 import git
 
+repo_url = 'http://pi.me.home:3000/fsadannn/covid19cubadata.github.io.git'
+#repo_url = 'https://github.com/covid19cuba/covid19cubadata.github.io.git'
 root = 'covid19cubadataactions'
-upload_root = root + '/api'
-entries = []
+upload_root = '/api'
 ftp_dir = '/htdocs/covid/'
-ftp_command = 'lftp -u {user},{password} -e "set ssl:verify-certificate false;{command}exit;" {host}'
-
+ftp_command = 'lftp -u {user},{password} -p {port} -e "set ssl:verify-certificate false;{command}exit;" {host}'
+mkdir_str = 'cd ' + ftp_dir + '{folder}' + ' || mkdir -p ' + ftp_dir + '{folder}' + ';cd ' + ftp_dir + '{folder}' + ';'
+branch='gh-pages'
 
 class LeaveMissing(dict):
     def __missing__(self, key):
         return '{' + key + '}'
 
+def get_last_commit():
+    # this is for avoid clonning if is up to date
+    r = git.Repo.init('temp_repo')
+    r.create_remote('origin', repo_url)
+    res = r.git.ls_remote(repo_url)
+    res = res.split('\n')
+    last_commit = ''
+    for i in res:
+        i=i.split('\t')
+        if branch in i[1]:
+            last_commit=branch[0]
+            break
+    r.close()
+    os.system('rm -rf temp_repo')
+    return last_commit
 
 def mkdir(folder):
-    return 'cd ' + ftp_dir + folder + ' || mkdir -p ' + ftp_dir + folder + ';cd ' + ftp_dir + folder + ';'
+    return mkdir_str.format(folder=folder)
 
 
-def walk_upload(folder):
-    if not os.path.isdir(folder):
+def walk_upload(root, folder):
+    if not os.path.isdir(root) and not os.path.isdir(folder):
         return
 
-    entries.append(folder)
-    while len(entries):
-        entry = entries.pop()
-        temp = str(entry).replace('covid19cubadataactions/', '')
-        print("Processing", temp)
-        os.system(ftp_command.format(command=mkdir(temp) + 'mput -P 2 ' + entry + '/*;'))
-        print("Uploaded", temp)
-        if os.path.isdir(entry):
-            items = os.listdir(entry)
-            for item in items:
-                item = entry + '/' + item
-                if os.path.isdir(item):
-                    entries.append(item)
+    with read_only(cache_directory(open_fs(root))) as home_fs:
+
+        for dir_path in home_fs.walk.dirs(folder):
+            print("Processing", dir_path)
+            entry=root+dir_path
+            os.system(ftp_command.format(command=mkdir(dir_path) + 'mput -P 2 ' + entry + '/*;'))
+            print("Uploaded", dir_path)
 
 
-def run_update(host, user, password):
+def run_update(host, user, password, port=21):
     global ftp_command
-    ftp_command = ftp_command.format_map(LeaveMissing(user=user, password=password, host=host))
+    ftp_command = ftp_command.format_map(LeaveMissing(user=user, password=password, host=host, port=port))
 
     last_commit = None
     if os.path.isfile('last_commit'):
         with open('last_commit', 'r') as file:
-            last_commit = file.read().replace('\n', '')
+            last_commit = file.readline().strip()
 
-    os.system('git clone -b gh-pages --depth=1 https://github.com/covid19cuba/covid19cubadata.github.io.git ' + root)
-
-    repo = git.Repo(root)
-    if str(repo.commit()) == last_commit:
+    remote_commit = get_last_commit()
+    if remote_commit == last_commit:
         print('Already up to date')
     else:
-        print('Will need to update', repo.commit(), last_commit)
-        walk_upload(upload_root)
+        os.system('git clone -b {0} --depth=1 {1} {2}'.format(branch,repo_url,root))
+        print('Will need to update', remote_commit, last_commit)
+        walk_upload(root, upload_root)
 
         with open('last_commit', 'w') as file:
-            file.write(str(repo.commit()))
+            file.write(remote_commit)
 
     os.system('rm -rf ' + root)
 
