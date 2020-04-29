0962f4ce53bd7221474df984be980e61da36673c
Sandy Fernando Perez Matamoros <sandypm007@gmail.com>
2020-04-29 13:04:21 -0400

    Final tweaks

--- ftp_upload_cli.py
+++ ftp_upload_cli.py
@@ -6,10 +6,9 @@ import git
 from fs import open_fs
 from fs.wrap import read_only, cache_directory
 
-repo_url = 'https://github.com/covid19cuba/covid19cubadata.github.io.git'
 root = 'covid19cubadataactions'
 
-ftp_dir = '/htdocs/covid'
+ftp_dir = '/htdocs/testing'
 ftp_command = 'lftp -u {user},{password} -p {port} -e "set ssl:verify-certificate false;{command}exit;" {host}'
 mkdir_str = 'cd ' + ftp_dir + '{folder}' + ' || mkdir -p ' + ftp_dir + '{folder}' + ';cd ' + ftp_dir + '{folder}' + ';'
 
@@ -19,7 +18,7 @@ class LeaveMissing(dict):
         return '{' + key + '}'
 
 
-def get_last_commit(branch):
+def get_last_commit(repo_url, branch):
     if os.path.exists('temp_repo'):
         os.system('rm -rf temp_repo')
     r = git.Repo.init('temp_repo')
@@ -38,26 +37,26 @@ def mkdir(folder):
     return mkdir_str.format(folder=folder)
 
 
-def walk_upload(root, folder):
-    if not os.path.isdir(root) and not os.path.isdir(folder):
+def walk_upload(root_folder, sub_folder):
+    if not os.path.isdir(root_folder) and not os.path.isdir(sub_folder):
         return
 
-    with read_only(cache_directory(open_fs(root))) as home_fs:
-        for dir_path in home_fs.walk.dirs(folder, exclude_dirs=["*.git", ".github"]):
+    with read_only(cache_directory(open_fs(root_folder))) as home_fs:
+        for dir_path in home_fs.walk.dirs(sub_folder, exclude_dirs=["*.git", ".github"]):
             print("Processing", dir_path)
-            entry = root + dir_path
+            entry = root_folder + dir_path
             os.system(ftp_command.format(command=mkdir(dir_path) + 'mput -P 2 ' + entry + '/*;'))
             print("Uploaded", dir_path)
 
 
-def update_branch(branch, upload_root):
+def update_branch(branch, upload_root, repo_url):
     last_commit_file = 'last_commit_' + branch
     last_commit = None
     if os.path.isfile(last_commit_file):
         with open(last_commit_file, 'r') as file:
             last_commit = file.readline().strip()
 
-    remote_commit = get_last_commit(branch)
+    remote_commit = get_last_commit(repo_url, branch)
     if remote_commit == last_commit:
         print('Already up to date')
     else:
@@ -75,8 +74,8 @@ def run_update(host, user, password, port=21):
     global ftp_command
     ftp_command = ftp_command.format_map(LeaveMissing(user=user, password=password, host=host, port=port))
 
-    update_branch('master', '/')
-    update_branch('gh-pages', '/api')
+    update_branch('master', '/', 'https://github.com/covid19cubadata/covid19cubadata.github.io.git')
+    update_branch('gh-pages', '/api', 'https://github.com/covid19cuba/covid19cubadata.github.io.git')
 
 
 if __name__ == "__main__":
